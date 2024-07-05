a2c1249ed5692e978f8bd130577243cad16bbb46
Joshua Ross <Rossj25@michigan.gov>
2024-07-05 12:03:29 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<p align="center">
+  <img src="./assets/images/github-header-image.png" alt="Header">
+  <a href="https://github.com/ColoredBytes/Sempahore/blob/96113c308c5c5c57bb28591d058b2e90b2c65d33/LICENSE">
+    <img src="https://img.shields.io/badge/License-MIT-yellow.svg" alt="License: MIT">
+  </a>
+</p>
+
+# :link: Table of Contents
+
++ [:link: Table of Contents](#%3A%6C%69%6E%6B%3A%2D%74%61%62%6C%65%2D%6F%66%2D%63%6F%6E%74%65%6E%74%73)
+  + [:x: Problem](#%3A%78%3A%2D%70%72%6F%62%6C%65%6D)
+  + [:heavy\_check\_mark: Solution](#%3A%68%65%61%76%79%5F%63%68%65%63%6B%5F%6D%61%72%6B%3A%2D%73%6F%6C%75%74%69%6F%6E)
+  + [:gear: Instructions](#%3A%67%65%61%72%3A%2D%69%6E%73%74%72%75%63%74%69%6F%6E%73)
+  + [:memo: Notes](#%3A%6D%65%6D%6F%3A%2D%6E%6F%74%65%73)
+
+
+## :x: Problem
+
+## :heavy_check_mark: Solution
+
+## :gear: Instructions
+
+## :memo: Notes
---
+++ install.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+# Dev: Joshua Ross
+# Github: https://github.com/ColoredBytes
+# Purpose: Graylog install script.
+
+# Variables
+HOST_IP=$(hostname -I | cut -d' ' -f1) # Get the IP address of the host machine
+TMP=$(mktemp -d) # Create TMP directory
+LOG_FILE="$TMP/errors.log"
+CURDIR=$(pwd)
+SUBFOLDER="conf"   # Replace this with the name of your subfolder
+
+# script functions
+error_exit() {
+    echo "$1" 1>&2
+    echo "An error occurred. Please check the log file at ${LOG_FILE} for more details."
+    exit 1
+
+
+monogo_systemd() {
+    sudo systemctl daemon-reload
+sudo systemctl enable mongod
+sudo systemctl start mongod
+sudo systemctl status mongod
+}
+
+graylog_systemd() {
+    sudo systemctl daemon-reload
+sudo systemctl enable graylog-server.service
+sudo systemctl start graylog-server.service
+sudo systemctl --type=service --state=active | grep graylog
+}
+
+OpenSearch_install() {
+    sudo curl -SL https://artifacts.opensearch.org/releases/bundle/opensearch/2.x/opensearch-2.x.repo -o /etc/yum.repos.d/opensearch-2.x.repo
+sudo sed -i "s/^gpgcheck=.*/gpgcheck=0/g" /etc/yum.repos.d/opensearch-2.x.repo
+sudo OPENSEARCH_INITIAL_ADMIN_PASSWORD=$(tr -dc A-Z-a-z-0-9_@#%^-_=+ < /dev/urandom  | head -c${1:-32}) yum -y install opensearch
+}
+
+# -----------------------------------------------------------------------------------------------
+
+echo "[+] Checking for root permissions"
+if [ "$EUID" -ne 0 ];then
+    echo "Please run this script as root"
+    exit 1
+fi
+
+echo "[+] Checking for updates"
+dnf upgrade -y
+
+# -----------------------------------------------------------------------------------------------
+
+# MongoDB Install
+mv "$CURDIR/$SUBFOLDER/mongodb-org.repo" /etc/yum.repos.d/mongodb-org.repo
+sudo yum install -y mongodb-org
+sudo yum versionlock add mongodb-org
+monogo_systemd
+
+# -----------------------------------------------------------------------------------------------
+
+# OpenSearch Install
+OpenSearch_install
+
+# Add OpenSearch Config.
+cat > /graylog/opensearch/config/opensearch.yml <<EOF
+cluster.name: graylog
+node.name: ${HOSTNAME}
+path.data: /var/lib/opensearch
+path.logs: /var/log/opensearch
+network.host: 0.0.0.0
+discovery.seed_hosts: ["127.0.0.1"]
+cluster.initial_master_nodes: ["127.0.0.1"]
+action.auto_create_index: false
+plugins.security.disabled: true
+indices.query.bool.max_clause_count: 32768
+EOF
+
+# Enable JVM options.
+sudo sed -i 's/-Xms[0-9]*g/-Xms8g/' /etc/opensearch/jvm.options
+sudo sed -i 's/-Xmx[0-9]*g/-Xmx8g/' /etc/opensearch/jvm.options
+
+# setting max files for opensearch
+echo "[+] setting max files for opensearch"
+sysctl -w vm.max_map_count=262144
+echo 'vm.max_map_count=262144' >> /etc/sysctl.conf
+
+# Restart Opensearch Service
+echo "[+] Reloading Opensearch Service"
+systemctl daemon-reload
+systemctl enable opensearch.service
+systemctl start opensearch.service
+
+# -----------------------------------------------------------------------------------------------
+
+# Graylog Install
+sudo rpm -Uvh https://packages.graylog2.org/repo/packages/graylog-6.0-repository_latest.rpm
+sudo yum install graylog-server
+sudo yum versionlock add graylog-server-6.0
+
+# Create Secrets
+SECRET=`pwgen -N 1 -s 96`
+echo -n "Enter Admin wenb interface Password: "
+read passwd
+ADMIN=`echo $passwd| tr -d '\n' | sha256sum | cut -d" " -f1`
+echo "Generated password salt is " $secret
+echo "Genberated admin hash is " $admin
+
+# Apply Secrets
+echo "[+] Adjusting Graylog Server configuration file"
+CONFIGSECRET=`echo "password_secret = "$SECRET`
+CONFIGADMIN=`echo "root_password_sha2 = "$ADMIN`
+echo "[+] replacing in configuration files"
+sed -r "s/password_secret =/${CONFIGSECRET}/g" -i /etc/graylog/server/server.conf
+sed -r "s/root_password_sha2 =/${CONFIGADMIN}/g" -i /etc/graylog/server/server.conf
+sed -i 's/#http_bind_address = 127.0.0.1:9000/http_bind_address = 0.0.0.0:9000/g' /etc/graylog/server/server.conf
+
+# Graylog systemd commands
+graylog_systemd
+
